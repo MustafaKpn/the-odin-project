09abb6a19748e3e060fcd061df04631b3f9c9ed3
Mustafa Kaplan <mustafa.kpn@outlook.com>
2026-05-08 20:45:29 +0300

    Feat: Created the footer loader

--- assignment5/restaurant-page/src/index.js
+++ assignment5/restaurant-page/src/index.js
@@ -3,8 +3,12 @@ import loadHome from "./modules/home.js";
 import loadMenu from "./modules/menu.js";
 import loadOurStory from "./modules/story.js";
 import loadReviews from "./modules/reviews.js";
+import loadContact from "./modules/contact.js";
+import loadFooter from "./modules/footer.js";
 
 loadHome();
 loadMenu();
 loadOurStory();
 loadReviews();
+loadContact();
+loadFooter();
